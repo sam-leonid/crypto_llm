3e1ed32274ac6c294a574596bd60da536c1bd607
sam-leonid <lsamorcev@gmail.com>
2024-08-30 15:35:18 +0300

    fixes cmc_info create

--- crypto_llm/storage.py
+++ crypto_llm/storage.py
@@ -44,7 +44,7 @@ class FileStorage(BaseStorage):
         self.cmc_detailed_info = (
             pd.read_csv(self.cmc_detailed_info_path)
             if os.path.exists(self.cmc_detailed_info_path)
-            else pd.DataFrame(columns=["symbol"])
+            else pd.DataFrame(columns=["symbol", "name"])
         )
         self.pdf_correct_names = pd.read_csv(self.path + "pdf_correct_names.csv")[
             "names"
